4a10b78b5e60c8c954ca3df4181504a5a243cb0b
Carlos Akión Garro Campos <carloscamp1008@estudianTEC.cr>
2022-08-27 14:30:45 -0600

    Update

--- Compiler/MonkeyGrammar.g4
+++ Compiler/MonkeyGrammar.g4
@@ -3,10 +3,7 @@ grammar MonkeyGrammar;
 
 
 //Productions
-
-
-
-
+startRule: identifier;
 
 //Tokens
 identifier:LETTER(LETTER|DIGIT)*;
--- Compiler/main.py
+++ Compiler/main.py
@@ -1,4 +1,5 @@
 from antlr4 import *
+import eel
 
 from Generated.MonkeyGrammarLexer import MonkeyGrammarLexer
 from Generated.MonkeyGrammarListener import MonkeyGrammarListener
@@ -7,13 +8,19 @@ from Generated.MonkeyGrammarVisitor import MonkeyGrammarVisitor
 from KeyPrinter import KeyPrinter
 
 
-def main():
-    input = 'test.txt'
-    input_stream = FileStream(input)
-    lexer = MonkeyGrammarLexer(input_stream)
-    token = CommonTokenStream(lexer)
-    parser = MonkeyGrammarParser(token)
-    tree = parser.startRule()
-    visitor = MonkeyGrammarVisitor()
-    visitor.visit(tree)
+
+eel.init('GUI')
+eel.start('index.html', mode='my_portable_chromium',
+                        host='localhost',
+                        port=27000,
+                        block=True )
+print("hola mundo")
+input = 'test.txt'
+input_stream = FileStream(input)
+lexer = MonkeyGrammarLexer(input_stream)
+token = CommonTokenStream(lexer)
+parser = MonkeyGrammarParser(token)
+tree = parser.startRule()
+visitor = MonkeyGrammarVisitor()
+visitor.visit(tree)
 
