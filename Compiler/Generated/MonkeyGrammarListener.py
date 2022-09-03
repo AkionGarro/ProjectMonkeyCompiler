df86740d6459d14b03675a1f46eac21b7c00d3bc
Carlos Akión Garro Campos <carloscamp1008@estudianTEC.cr>
2022-09-03 15:24:13 -0600

    Update

---
+++ Compiler/Generated/MonkeyGrammarListener.py
@@ -0,0 +1,39 @@
+# Generated from C:/Users/garroakion/Desktop/Projects2Semestre/Compiladores/ProjectMonkeyCompiler/Compiler\MonkeyGrammar.g4 by ANTLR 4.10.1
+from antlr4 import *
+if __name__ is not None and "." in __name__:
+    from .MonkeyGrammarParser import MonkeyGrammarParser
+else:
+    from MonkeyGrammarParser import MonkeyGrammarParser
+
+# This class defines a complete listener for a parse tree produced by MonkeyGrammarParser.
+class MonkeyGrammarListener(ParseTreeListener):
+
+    # Enter a parse tree produced by MonkeyGrammarParser#startRule.
+    def enterStartRule(self, ctx:MonkeyGrammarParser.StartRuleContext):
+        pass
+
+    # Exit a parse tree produced by MonkeyGrammarParser#startRule.
+    def exitStartRule(self, ctx:MonkeyGrammarParser.StartRuleContext):
+        pass
+
+
+    # Enter a parse tree produced by MonkeyGrammarParser#identifier.
+    def enterIdentifier(self, ctx:MonkeyGrammarParser.IdentifierContext):
+        pass
+
+    # Exit a parse tree produced by MonkeyGrammarParser#identifier.
+    def exitIdentifier(self, ctx:MonkeyGrammarParser.IdentifierContext):
+        pass
+
+
+    # Enter a parse tree produced by MonkeyGrammarParser#char.
+    def enterChar(self, ctx:MonkeyGrammarParser.CharContext):
+        pass
+
+    # Exit a parse tree produced by MonkeyGrammarParser#char.
+    def exitChar(self, ctx:MonkeyGrammarParser.CharContext):
+        pass
+
+
+
+del MonkeyGrammarParser
